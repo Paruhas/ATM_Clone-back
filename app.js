f68ff62c0f64ba6a7b21dbdc72772be4967c1390
apbank2238 <bayulgang@hotmail.com>
2021-06-15 02:50:35 +0700

    create controller and route for other 2 model

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const cors = require("cors");
 const { errors } = require("./middlewares/errors");
 
 const userRouter = require("./routes/userRouter");
-// const transactionRouter = require("./routes/transactionRouter");
-// const cashRouter = require("./routes/cashRouter");
+const transactionRouter = require("./routes/transactionRouter");
+const cashRouter = require("./routes/cashRouter");
 
 const app = express();
 
@@ -14,8 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/", userRouter);
-// app.use("/transaction", transactionRouter);
-// app.use("/cash", cashRouter);
+app.use("/transaction", transactionRouter);
+app.use("/cash", cashRouter);
 
 app.use("/", (req, res, next) => {
   res.status(404).json({ message: "Path not found" });
